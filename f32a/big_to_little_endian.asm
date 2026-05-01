8d0041f6676d6a7603dc767ce02147b413f1b792
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-05-02 01:19:05 +0300

    refactor(f32a): big_to_little_endian algorithm realization

---
+++ f32a/big_to_little_endian.asm
@@ -0,0 +1,58 @@
+.data
+.org 0x00
+
+    \; I/O consts
+    input_addr:     .word   0x80    \; input address
+    output_addr:    .word   0x84    \; output address
+
+    \; programm consts
+    mask:           .word   0xFF    \; mask for byte highlighting
+    move_bytes:     .word   4       \; number of bytes not shifted
+    current_byte:   .word   0       \; value of working byte
+
+.text
+.org 0x90
+
+    shift_right_8:                  \; implementation of n >> 8
+        2/ 2/ 2/ 2/ 2/ 2/ 2/ 2/ ;
+
+    shift_left_8:                   \; implementation of n << 8
+        2* 2* 2* 2* 2* 2* 2* 2* ;
+
+    decrement_move_bytes:
+        @p move_bytes               \; stack <- mem[move_bytes]
+        lit -1 +                    \; stack <- -1 && (+ mem[move_bytes] -1)
+        !p move_bytes ;             \; mem[move_bytes] <- stack.top
+
+
+    _start:
+        @p  input_addr              \; stack <- mem[input_addr] <=> stack <- 0x80
+        b!                          \; A register <- stack.top <=> A register <- 0x80
+        @b                          \; stack <- mem[A] <=> stack <- mem[0x80]
+
+
+    preloop_check:
+        @p move_bytes               \; stack <- mem[move_bytes]
+        if _finish                  \; if (move_bytes == 0) => _finish
+
+    loop:
+        \; select junior byte of the number
+        dup
+        @p mask and
+        !p current_byte
+
+        \; move result << 8 & add our current working byte
+        a shift_left_8
+        @p current_byte +
+        a!
+
+        \; preparation before next loop step
+        shift_right_8
+        decrement_move_bytes
+        preloop_check ;
+
+
+    _finish:
+        @p output_addr b!       \; stack <- 0x84 && B register <- 0x84
+        a !b                    \; stack <- A register && mem[B] <- stack.top
+        halt
